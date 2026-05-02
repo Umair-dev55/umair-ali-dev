1d70a71e43e09f4f61b5874bea67afb6f1266221
Umair Dev <umair.personal55@gmail.com>
2026-05-02 15:17:15 +0500

    Meta data change

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="p-3 text-center rounded-b-3xl bg-accent">Header</div>;
+  return (
+    <div className="mx-auto -z-10 w-[1130px] absolute p-3 text-center rounded-b-3xl bg-accent">
+      Header
+    </div>
+  );
 };
 
 export default Header;
