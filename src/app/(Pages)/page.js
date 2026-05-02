98425af4cb6668f5719045856d946771e2d922a1
Umair Dev <umair.personal55@gmail.com>
2026-05-02 15:00:30 +0500

    Add Blob Animations & img

--- src/app/(Pages)/page.js
+++ src/app/(Pages)/page.js
@@ -1,6 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
 import HeroImage from "../Components/HeroImage";
+import Image from "next/image";
+import myImg from "../../../public/assets/png.png";
 const Home = () => {
   return (
     <motion.section
@@ -12,10 +14,29 @@ const Home = () => {
       className="h-screen flex items-center"
     >
       <div className="flex flex-col xl:flex-row items-center justify-between w-full">
-        <div className="w-full xl:w-[550px]">Text</div>
+        <div className="w-full xl:w-[550px]">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Necessitatibus voluptate iure asperiores fuga. In blanditiis debitis
+          vel praesentium nisi pariatur, laborum minima facilis eum libero hic
+          iusto repellendus exercitationem deleniti necessitatibus beatae sunt
+          nam magni, ab dolor. Deleniti officiis dolorem culpa commodi
+          voluptates ratione illum nisi accusantium. Qui, incidunt. Magnam.
+        </div>
         <div className="hidden xl:block flex-1 relative z-20">
           {/* blob */}
           <HeroImage />
+          {/* img */}
+          <Image
+            src={myImg}
+            alt="image"
+            width={400}
+            height={300}
+            quality={100}
+            className="absolute top-20 right-10 rounded-full"
+          />
+          {/* overlay */}
+          <div className="absolute top-0 right-0 w-full h-full bg-primary/20 opacity-50 rounded-full z-10"></div>
         </div>
       </div>
     </motion.section>
--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="p-10 bg-accent">Header</div>;
+  return <div className="p-3 text-center rounded-b-3xl bg-accent">Header</div>;
 };
 
 export default Header;
