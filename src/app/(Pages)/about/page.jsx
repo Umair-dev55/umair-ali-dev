2adc8177b3e3d77c83af75db2e36bbbd293d1214
Umair Dev <umair.personal55@gmail.com>
2026-05-10 02:11:50 +0500

    About Page Styling + Shadcn Ui component

--- src/app/(Pages)/about/page.jsx
+++ src/app/(Pages)/about/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import HeroImageCard from "@/components/HeroImageCard";
 const about = () => {
   return (
     <motion.section
@@ -8,9 +10,45 @@ const about = () => {
         opacity: 1,
         transition: { delay: 1.5, duration: 0.4, ease: "easeIn" },
       }}
-      className="h-screen flex items-center"
+      className="py-15 sm:py-0"
     >
-      about
+      <div className="grid p-2 sm:grid-cols-2 min-h-screen place-items-center">
+        {/* 1st div */}
+        <div className="">
+          <HeroImageCard />
+        </div>
+        {/* 2nd div */}
+
+        <div className="text-center md:text-left">
+          <div className="flex justify-evenly flex-col items-center md:items-start gap-6">
+            <ScrollArea className="h-[500px] w-full p-4">
+              <div className="">
+                <div className="flex items-center gap-2">
+                  <div className="w-2 h-2 bg-accent rounded-full"></div>
+                  <p>About Me</p>
+                </div>
+                <h2 className="h2 mb-6">
+                  <span className="text-accent">Umair</span>/Dev
+                </h2>
+                <p className="">
+                  I am a frontend web developer specializing in building modern,
+                  responsive, and user-friendly interfaces. I am passionate
+                  about web development and continuously strive to improve my
+                  skills and knowledge in the ever-evolving field of web
+                  technologies.
+                </p>
+                <div className="flex w-full  h-[2px] bg-gray-400">
+                  <div className="w-1/2 ">State</div>
+                  <div className="w-1/2 ">Testimonial</div>
+                  <div className="w-1/2 ">Info</div>
+                  <div className="w-1/2 ">Journey</div>
+                </div>
+              </div>
+              <ScrollBar orientation="vertical" />
+            </ScrollArea>
+          </div>
+        </div>
+      </div>
     </motion.section>
   );
 };
