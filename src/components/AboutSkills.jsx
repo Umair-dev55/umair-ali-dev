1526b50dd967a8fdb84cad35a8c0272db79ab1d5
Umair Dev <umair.personal55@gmail.com>
2026-05-11 18:58:18 +0500

    Demo Components

---
+++ src/components/AboutSkills.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutSkills = () => {
+  return <div>Skills</div>;
+};
+
+export default AboutSkills;
---
+++ src/components/AboutTestimonials.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutTestimonials = () => {
+  return <div>Testimonials</div>;
+};
+
+export default AboutTestimonials;
