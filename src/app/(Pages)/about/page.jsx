dbe9920f190655f0f4a90d3db23288ffe20cb066
Umair Dev <umair.personal55@gmail.com>
2026-05-11 18:57:06 +0500

    About Page Responsiveness + About me done + ADD Countup states done

--- src/app/(Pages)/about/page.jsx
+++ src/app/(Pages)/about/page.jsx
@@ -2,6 +2,10 @@
 import { motion } from "framer-motion";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import HeroImageCard from "@/components/HeroImageCard";
+import AboutStats from "@/components/AboutStats";
+import AboutTestimonials from "@/components/AboutTestimonials";
+import AboutInfo from "@/components/AboutInfo";
+import AboutSkills from "@/components/AboutSkills";
 const about = () => {
   return (
     <motion.section
@@ -12,23 +16,24 @@ const about = () => {
       }}
       className="py-15 sm:py-0"
     >
-      <div className="grid p-2 sm:grid-cols-2 min-h-screen place-items-center">
+      <div className="grid  sm:grid-cols-2 min-h-screen place-items-center">
         {/* 1st div */}
-        <div className="">
+        <div className="relative py-10">
           <HeroImageCard />
         </div>
         {/* 2nd div */}
 
-        <div className="text-center md:text-left">
-          <div className="flex justify-evenly flex-col items-center md:items-start gap-6">
-            <ScrollArea className="h-[500px] w-full p-4">
-              <div className="">
+        <div className="text-left ">
+          <ScrollArea className="h-[500px] w-full">
+            <div className="flex justify-evenly flex-col items-start gap-6">
+              <div className="text-start *:py-2">
                 <div className="flex items-center gap-2">
-                  <div className="w-2 h-2 bg-accent rounded-full"></div>
-                  <p>About Me</p>
+                  <span className="w-2 h-2 bg-accent rounded-full"></span>
+                  <span>About Me</span>
                 </div>
-                <h2 className="h2 mb-6">
-                  <span className="text-accent">Umair</span>/Dev
+                <h2 className="">
+                  <span className="h1 text-accent">Umair</span>
+                  <span>/Dev</span>
                 </h2>
                 <p className="">
                   I am a frontend web developer specializing in building modern,
@@ -37,16 +42,25 @@ const about = () => {
                   skills and knowledge in the ever-evolving field of web
                   technologies.
                 </p>
-                <div className="flex w-full  h-[2px] bg-gray-400">
-                  <div className="w-1/2 ">State</div>
-                  <div className="w-1/2 ">Testimonial</div>
-                  <div className="w-1/2 ">Info</div>
-                  <div className="w-1/2 ">Journey</div>
+              </div>
+              <div className="h-[2px] w-full bg-gray-400"></div>
+              <div className="flex flex-col ">
+                <div className="">
+                  <AboutStats />
+                </div>
+                <div className="">
+                  <AboutTestimonials />
+                </div>
+                <div className="">
+                  <AboutInfo />
+                </div>
+                <div className="">
+                  <AboutSkills />
                 </div>
               </div>
-              <ScrollBar orientation="vertical" />
-            </ScrollArea>
-          </div>
+            </div>
+            <ScrollBar orientation="vertical" />
+          </ScrollArea>
         </div>
       </div>
     </motion.section>
