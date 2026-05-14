b4950a97debce6f1c223e2a0986d6849edb2f78d
Umair Dev <umair.personal55@gmail.com>
2026-05-14 23:44:53 +0700

    Animation speed optimize

--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -25,7 +25,7 @@ const Rectangle = () => {
         exit="exit"
         transition={{
           delay: 0,
-          duration: 0.6,
+          duration: 0.1,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-30 bg-[#1b162b]"
@@ -36,8 +36,8 @@ const Rectangle = () => {
         animate="animate"
         exit="exit"
         transition={{
-          delay: 0.25,
-          duration: 0.8,
+          delay: 0.1,
+          duration: 0.4,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-30 bg-[#241e36]"
@@ -49,7 +49,7 @@ const Rectangle = () => {
         exit="exit"
         transition={{
           delay: 0.35,
-          duration: 1,
+          duration: 0.4,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-10 bg-[#241e36]"
@@ -60,8 +60,8 @@ const Rectangle = () => {
         animate="animate"
         exit="exit"
         transition={{
-          delay: 0.35,
-          duration: 1,
+          delay: 0.1,
+          duration: 0.4,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-10 bg-[#2e2743]"
