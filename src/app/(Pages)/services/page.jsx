3b03028ac8c71b13d0283fefa4cde00ff34a51d6
Umair Dev <umair.personal55@gmail.com>
2026-05-18 21:17:38 +0700

    Complete Services page 1- Slider Dynamic

--- src/app/(Pages)/services/page.jsx
+++ src/app/(Pages)/services/page.jsx
@@ -1,7 +1,52 @@
 "use client";
 import { motion } from "framer-motion";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import HeroImageCard from "@/components/HeroImageCard";
+import { FaReact, FaWordpress } from "react-icons/fa";
+import { SiNextdotjs, SiTailwindcss, SiFramer } from "react-icons/si";
+import { SiSitecore } from "react-icons/si";
+import { GoArrowUpRight } from "react-icons/go";
+import { MdArrowRightAlt } from "react-icons/md";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+const UserData = [
+  {
+    title: "Frontend Developer",
+    desc: "HTML, CSS, JavaScript, React JS, Next JS, Tailwind CSS",
+    icon: FaReact,
+  },
+  {
+    title: "React Developer",
+    desc: "React JS, Hooks, State Management, API Integration",
+    icon: FaReact,
+  },
+  {
+    title: "Next.js Developer",
+    desc: "SSR, SSG, API Routes, App Router",
+    icon: SiNextdotjs,
+  },
+  {
+    title: "Sitecore Developer",
+    desc: "Headless CMS, SXA, Components, JSS",
+    icon: SiSitecore,
+  },
+
+  {
+    title: "UI Developer",
+    desc: "Tailwind CSS, Responsive Design, UI Systems",
+    icon: SiTailwindcss,
+  },
+  {
+    title: "WordPress Developer",
+    desc: "Themes, Plugins, Customization, Elementor",
+    icon: FaWordpress,
+  },
+  {
+    title: "Animation Developer",
+    desc: "GSAP, Framer Motion, Micro interactions",
+    icon: SiFramer,
+  },
+];
 const services = () => {
   return (
     <motion.section
@@ -10,44 +55,63 @@ const services = () => {
         opacity: 1,
         transition: { delay: 1.5, duration: 0.4, ease: "easeIn" },
       }}
-      className="py-15 sm:py-0"
+      className="sm:py-0 flex-y-center min-h-screen px-2"
     >
-      <div className="grid p-5 sm:grid-cols-2 min-h-screen place-items-center">
-        {/* 1st div */}
-        <div className="">
-          <HeroImageCard />
+      <div className="container mx-auto w-full flex flex-col gap-16">
+        {/* text */}
+        <div className="flex flex-col xl:flex-row  gap-8 items-start xl:justify-between xl:items-center">
+          {/* heading */}
+          <h2 className="h2 max-w-[450px] text-left xl:mb-0">
+            Custom <span className="text-accent">Web Solutions</span> to boost
+            your business
+          </h2>
+          {/* btn  */}
+          <button className="btn btn-lg btn-accent flex gap-2">
+            All Services <MdArrowRightAlt />{" "}
+          </button>
         </div>
-        {/* 2nd div */}
-
-        <div className="text-center md:text-left">
-          <div className="flex justify-evenly flex-col items-center md:items-start gap-6">
-            <h1 className="h1">
-              Hi ! I Am Umair Ali,
-              <br />
-            </h1>{" "}
-            <h1 className="h1  text-accent font-extrabold"></h1>
-            <p className="para">
-              Passionate services building fast, intuitive, and visually
-              stunning web experiences using modern technologies.
-            </p>
-            <div className="hidden">
-              <div className="md:flex gap-4 text-lg">
-                {/* Phone */}
-                <div className="flex gap-2 justify-center lg:justify-start">
-                  <span className="text-accent"></span>
-                  <span>+92 308 6754044</span>
+        {/* slide  */}
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={20}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Autoplay]}
+          className="w-full ]"
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            640: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {UserData.map((item, index) => (
+            <SwiperSlide key={index} className="h-[284px]">
+              <div className="rounded-2xl custom-border h-[284px] flex  flex-col justify-between p-6">
+                <div className="flex justify-between">
+                  <item.icon className="w-12 h-12 text-accent" />
+                  <GoArrowUpRight className="bg-accent size-12 rounded-full" />
                 </div>
-                {/* mail */}
-                <div className="hidden lg:flex gap-2">
-                  <span className="text-accent"></span>
-                  <span className="">umair.dev55@gmail.com</span>
+                <div>
+                  <h2 className="h3">{item.title}</h2>
+                </div>
+                <div>
+                  <p className="font-light  w-full">{item.desc}</p>
                 </div>
               </div>
-            </div>
-            {/* Socials */}
-            <div className="md:absolute md:p-5 top-1/2 right-0 transform -translate-y-1/2"></div>
-          </div>
-        </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </motion.section>
   );
