e667ecfad8388822170579607c3e1ba61dc32a40
Umair Dev <umair.personal55@gmail.com>
2026-05-20 09:02:18 +0700

    responsive

--- src/components/Gradiant.jsx
+++ src/components/Gradiant.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Gradiant = () => {
   return (
-    <div className=" w-120 h-120 rounded-full sm:bg-accent/60 absolute top-[-10%] right-[-4%] z-10 blur-[400px]" />
+    <div className=" w-120 h-120 rounded-full sm:bg-accent/60 absolute top-[-10%] right-[-4%] z-10 blur-[400px] pointer-events-none" />
   );
 };
 
