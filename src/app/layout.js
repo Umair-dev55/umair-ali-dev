5fb6c067d96756439251abb3cd40fe1b8f696900
Umair Dev <umair.personal55@gmail.com>
2026-05-31 17:28:21 +0700

    add in search console

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,17 +6,20 @@ import RectangleTransition from "@/components/RectangleTransition";
 import PageTransition from "@/components/PageTransition";
 import MainNav from "@/components/MainNav";
 import Header from "@/components/Header";
+export const metadata = {
+  title: "Umair Ali - Portfolio",
+  description:
+    "React/Next.js Developer Experienced in building responsive and user-friendly web applications.",
+  verification: {
+    google: "2UCnFO3Hs6Ui_qLcYaGkbJkpKy27W03S0eKMkFPliJU",
+  },
+};
 
 const cinzel = Cinzel({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"], // multiple weights available
   display: "swap",
 });
-export const metadata = {
-  title: "Umair Ali - Portfolio",
-  description:
-    "React/Next.js Developer Experienced in building responsive and user-friendly web applications.",
-};
 
 export default function RootLayout({ children }) {
   return (
